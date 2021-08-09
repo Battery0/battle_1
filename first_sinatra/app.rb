c62be568b6c1cef22acc347911a894bc27e5eed9
ian k <kickthechair@gmail.com>
2021-08-09 16:40:31 +0100

    red border dashed kitty

---
+++ first_sinatra/app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  "Hello World"
+end
+
+get '/cat' do
+  "<div style ='border: dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  <div>"
+end
+
